ab5fc73a39de6f1de6ea3d6671bdb03d845e4ee5
Will James <jameswj@miamioh.edu>
2021-07-24 20:24:02 -0500

    add chessboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import userEvent from '@testing-library/user-event';
-import { userInfo } from 'os';
-
-interface State {
-  users: User[]
-}
-
-interface User {
-  username: string,
-  id: number,
-}
+import { ChessGame } from "./components/ChessGame";
 
 class App extends Component {
-  state: State = { users: [] };
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
   render() { 
     return (
       <div className="App">
-        <h1>Users</h1>
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}>{user.username}</li>
-          )}
-        </ul>
+        <ChessGame />
       </div>
     );
   }
